7d88a955472a6836f0b85f91370e03a5ddaea53f
lostyway <hardsheller@gmail.com>
2025-07-03 06:29:55 +0300

    fix (нужно добавить JWT в след. итерации)

--- src/main/java/com/lostway/eventmanager/controller/UserAuthenticationController.java
+++ src/main/java/com/lostway/eventmanager/controller/UserAuthenticationController.java
@@ -1,11 +1,11 @@
 package com.lostway.eventmanager.controller;
 
-import com.lostway.eventmanager.controller.dto.JwtTokenResponse;
 import com.lostway.eventmanager.controller.dto.UserLoginDto;
 import com.lostway.eventmanager.controller.dto.UserRegistryDto;
 import com.lostway.eventmanager.controller.dto.UserToShowDto;
 import com.lostway.eventmanager.exception.UserAlreadyExistException;
 import com.lostway.eventmanager.mapper.UserMapper;
+import com.lostway.eventmanager.security.JwtAuthenticationResponse;
 import com.lostway.eventmanager.service.UserService;
 import com.lostway.eventmanager.service.model.UserModel;
 import jakarta.validation.Valid;
@@ -34,7 +34,7 @@ public class UserAuthenticationController {
     }
 
     @PostMapping("/auth")
-    public JwtTokenResponse authenticateUser(@RequestBody @Valid UserLoginDto userToLoginDto) {
+    public JwtAuthenticationResponse authenticateUser(@RequestBody @Valid UserLoginDto userToLoginDto) {
         return null;
     }
 
--- src/main/java/com/lostway/eventmanager/controller/dto/JwtTokenResponse.java
+++
@@ -1,8 +0,0 @@
-package com.lostway.eventmanager.controller.dto;
-
-import lombok.Data;
-
-@Data
-public class JwtTokenResponse {
-    private String token;
-}
