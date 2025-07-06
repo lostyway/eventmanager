f00da51da79d63c8c63114efb083982970010b83
lostyway <hardsheller@gmail.com>
2025-07-06 19:37:17 +0300

    небольшие тесты на TestContainers

--- src/test/java/com/lostway/eventmanager/repository/LocationRepositoryTest.java
+++ src/test/java/com/lostway/eventmanager/repository/LocationRepositoryTest.java
@@ -2,6 +2,7 @@ package com.lostway.eventmanager.repository;
 
 import com.lostway.eventmanager.IntegrationTestBase;
 import com.lostway.eventmanager.repository.entity.LocationEntity;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -12,21 +13,68 @@ class LocationRepositoryTest extends IntegrationTestBase {
     @Autowired
     private LocationRepository locationRepository;
 
-    @Test
-    void testLocationRepository() {
-        var locationEntity = LocationEntity.builder()
+    private LocationEntity locationTest;
+    private LocationEntity locationTestSecond;
+
+    @BeforeEach
+    void setUp() {
+        locationTest = LocationEntity.builder()
                 .name("locationTest")
                 .address("AddressTest")
                 .capacity(20)
                 .description("DescriptionTest")
                 .build();
 
-        var savedEntity = locationRepository.save(locationEntity);
+        locationTestSecond = LocationEntity.builder()
+                .name("locationTest")
+                .address("AddressTest")
+                .capacity(20)
+                .description("DescriptionTest")
+                .build();
+    }
+
+    @Test
+    void testLocationRepository() {
+        var savedEntity = locationRepository.save(locationTest);
         var locationFindByIdOpt = locationRepository.findById(savedEntity.getId());
 
         assertThat(locationFindByIdOpt.isPresent()).isTrue();
-        assertThat(locationFindByIdOpt.get().getName()).isEqualTo(locationEntity.getName());
-        assertThat(locationFindByIdOpt.get().getCapacity()).isEqualTo(locationEntity.getCapacity());
+        assertThat(locationFindByIdOpt.get().getName()).isEqualTo(locationTest.getName());
+        assertThat(locationFindByIdOpt.get().getCapacity()).isEqualTo(locationTest.getCapacity());
     }
 
+    @Test
+    void whenDeleteLocationThenSuccess() {
+        var savedEntity = locationRepository.save(locationTest);
+        locationRepository.delete(savedEntity);
+        var findDeleteOpt = locationRepository.findById(savedEntity.getId());
+
+        assertThat(findDeleteOpt.isPresent()).isFalse();
+    }
+
+    @Test
+    void whenDeleteLocationThenFailedThenFindEntity() {
+        var savedEntity = locationRepository.save(locationTest);
+        var savedEntityTwo = locationRepository.save(locationTestSecond);
+        locationRepository.delete(savedEntityTwo);
+
+        var findDeleteOpt = locationRepository.findById(savedEntity.getId());
+        var findDeleteOptTwo = locationRepository.findById(savedEntityTwo.getId());
+
+        assertThat(findDeleteOpt.isPresent()).isTrue();
+        assertThat(findDeleteOptTwo.isPresent()).isFalse();
+    }
+
+    @Test
+    void whenFindAllLocations() {
+        var listEntity = locationRepository.findAll();
+        assertThat(listEntity).size().isEqualTo(0);
+
+        locationRepository.save(locationTest);
+        locationRepository.save(locationTestSecond);
+
+        var listEntityFinalWithEntity = locationRepository.findAll();
+
+        assertThat(listEntityFinalWithEntity).size().isEqualTo(2);
+    }
 }
