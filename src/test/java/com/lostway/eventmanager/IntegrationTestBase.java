a63e4eae64e4b0d336a0796d78cada571972d355
lostyway <hardsheller@gmail.com>
2025-07-06 16:33:01 +0300

    Подкрутил TestContainers

---
+++ src/test/java/com/lostway/eventmanager/IntegrationTestBase.java
@@ -0,0 +1,27 @@
+package com.lostway.eventmanager;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@SpringBootTest
+@Transactional
+public class IntegrationTestBase {
+
+    public static final PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:15");
+
+    @BeforeAll
+    static void runContainer() {
+        container.start();
+    }
+
+    @DynamicPropertySource
+    static void postgresProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.username", container::getUsername);
+        registry.add("spring.datasource.password", container::getPassword);
+    }
+}
---
+++ src/test/java/com/lostway/eventmanager/repository/LocationRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.lostway.eventmanager.repository;
+
+import com.lostway.eventmanager.IntegrationTestBase;
+import com.lostway.eventmanager.repository.entity.LocationEntity;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LocationRepositoryTest extends IntegrationTestBase {
+
+    @Autowired
+    private LocationRepository locationRepository;
+
+    @Test
+    void testLocationRepository() {
+        LocationEntity locationEntity = LocationEntity.builder()
+                .name("locationTest")
+                .address("AddressTest")
+                .capacity(20)
+                .description("DescriptionTest")
+                .build();
+
+        LocationEntity savedEntity = locationRepository.save(locationEntity);
+        var locationFindByIdOpt = locationRepository.findById(savedEntity.getId());
+        assertThat(locationFindByIdOpt.isPresent()).isTrue();
+        assertThat(locationFindByIdOpt.get().getName()).isEqualTo(locationEntity.getName());
+        assertThat(locationFindByIdOpt.get().getCapacity()).isEqualTo(locationEntity.getCapacity());
+    }
+
+}
