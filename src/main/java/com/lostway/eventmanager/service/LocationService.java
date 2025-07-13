5ea63e939d7ae572bd9eebbc4904fac2ad2ad2e6
lostway <hardsheller@gmail.com>
2025-07-13 20:52:32 +0300

    logger fix

--- src/main/java/com/lostway/eventmanager/service/LocationService.java
+++ src/main/java/com/lostway/eventmanager/service/LocationService.java
@@ -9,7 +9,6 @@ import com.lostway.eventmanager.repository.LocationRepository;
 import com.lostway.eventmanager.repository.entity.LocationEntity;
 import com.lostway.eventmanager.service.model.Location;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -17,7 +16,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Slf4j
 @RequiredArgsConstructor
 public class LocationService {
     private final LocationMapper mapper;
